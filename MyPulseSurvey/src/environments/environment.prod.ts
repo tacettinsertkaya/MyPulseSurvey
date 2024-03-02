97edf015de5ecae590f2d57917af744444d02ccb
Tacettin Sertkaya <tacettinsertkaya@outlook.com>
2024-03-02 20:27:24 +0300

    updated

--- MyPulseSurvey/src/environments/environment.prod.ts
+++ MyPulseSurvey/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
    // serverBaseUrl: "https://localhost:5001/api",
   // appUrl:"http://localhost:4200/#/",
-  serverBaseUrl: "https://api.mypulsepro.com/api",
-  appUrl:"https://mypulsepro.com/#/"
+  serverBaseUrl: "https://api.hpwsurvey.com/api",
+  appUrl:"https://hpwsurvey.com/#/"
 };
